f3cf553f93c7ee431747cba5914cbfc49a8cb5ed
pbalduino <pbalduino@pftech.com.br>
2010-12-30 21:51:58 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarq::Application.config.secret_token = 'cc2780f47a844ee7e74234c439f93631a0d170fdcc84dbed50efc95310893a6027a958c1b72a9522affcec411f31ba56e148727210a36bf23db779c8d4be56c3'
